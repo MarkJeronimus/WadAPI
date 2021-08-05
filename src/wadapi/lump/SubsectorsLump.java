f14248b5eead5159aec20103d49f52a0afafc159
Mark Jeronimus <mark.jeronimus@gmail.com>
2021-08-05 21:50:31 +0200

    Encapsulate SubsectorsLump

--- src/wadapi/lump/SubsectorsLump.java
+++ src/wadapi/lump/SubsectorsLump.java
@@ -1,7 +1,9 @@
 package wadapi.lump;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+
+import static org.digitalmodular.utilities.ValidatorUtilities.requireNonNull;
 
 import wadapi.LumpType;
 import wadapi.structure.Subsector;
@@ -11,8 +13,8 @@ import static wadapi.LumpType.SSECTORS;
  * @author Zom-B
  */
 // Created 2018-01-21
-public class SubsectorsLump extends Lump {
-	private final List<Subsector> subsectors;
+public class SubsectorsLump extends Lump implements Iterable<Subsector> {
+	private final ArrayList<Subsector> subsectors;
 
 	public SubsectorsLump(String name, int initialCapacity) {
 		super(name);
@@ -20,12 +22,37 @@ public class SubsectorsLump extends Lump {
 		subsectors = new ArrayList<>(initialCapacity);
 	}
 
-	public List<Subsector> getSubsectors() {
-		return subsectors;
-	}
-
 	@Override
 	public LumpType getLumpType() {
 		return SSECTORS;
+	}
+
+	public int numVertices() {
+		return subsectors.size();
+	}
+
+	public Subsector get(int index) {
+		return subsectors.get(index);
+	}
+
+	public void add(Subsector subsector) {
+		subsectors.add(requireNonNull(subsector, "subsector"));
+	}
+
+	public void set(int index, Subsector subsector) {
+		subsectors.set(index, requireNonNull(subsector, "subsector"));
+	}
+
+	public void remove(int index) {
+		subsectors.remove(index);
+	}
+
+	public void ensureCapacity(int newCapacity) {
+		subsectors.ensureCapacity(newCapacity);
+	}
+
+	@Override
+	public Iterator<Subsector> iterator() {
+		return subsectors.iterator();
 	}
 }
